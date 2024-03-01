521b46c684a473753ec0b18ab7fc104d8c929367
svens0n58 <svensnel@gmail.com>
2024-03-01 21:10:06 +0100

    fixed flake

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from src.expression import LogicalExpression
 
 expression = LogicalExpression("A <-> B")
 expression.truth_table()
-expression.evaluate_expression(A = 1, B = 0)
+expression.evaluate_expression(A=1, B=0)
